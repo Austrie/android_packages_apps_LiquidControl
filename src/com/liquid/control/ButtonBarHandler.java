AOKP/packages_apps_ROMControl

update RC to use fragments and headers to be tablet friendly

src/com/roman/romcontrol/ButtonBarHandler.java
@@ -1,0 +1,30 @@
+
+package com.roman.romcontrol;
+
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.widget.Button;
+
+/**
+ * Interface letting {@link SettingsPreferenceFragment} access to bottom bar inside
+ * {@link android.preference.PreferenceActivity}.
+ */
+public interface ButtonBarHandler {
+    public boolean hasNextButton();
+
+    public Button getNextButton();
+}

src/com/roman/romcontrol/DialogCreatable.java
@@ -1,0 +1,30 @@
+package com.roman.romcontrol;
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+import android.app.Dialog;
+
+/**
+ * Letting the class, assumed to be Fragment, create a Dialog on it. Should be useful
+ * you want to utilize some capability in {@link SettingsPreferenceFragment} but don't want
+ * the class inherit the class itself (See {@link ProxySelector} for example).
+ */
+public interface DialogCreatable {
+
+    public Dialog onCreateDialog(int dialogId);
+}

src/com/roman/romcontrol/fragments/Led.java
@@ -1,0 +1,104 @@
+
+package com.roman.romcontrol.fragments;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.roman.romcontrol.R;
+import com.roman.romcontrol.SettingsPreferenceFragment;
+
+public class Led extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    public static final String TAG = "LEDPreferences";
+
+    private static final String PREF_LED_OFF = "led_off";
+    private static final String PREF_LED_ON = "led_on";
+    private static final String PREF_COLOR_PICKER = "led_color";
+
+    ListPreference mLedOffTime;
+    ListPreference mLedOnTime;
+    ColorPickerPreference mColorPicker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs_led);
+
+        mLedOffTime = (ListPreference) findPreference(PREF_LED_OFF);
+        mLedOffTime.setOnPreferenceChangeListener(this);
+        String ledOffTime = Settings.System
+                .getInt(getActivity().getContentResolver(),
+                        Settings.System.NOTIFICATION_LIGHT_OFF,
+                        getActivity()
+                                .getResources()
+                                .getInteger(
+                                        com.android.internal.R.integer.config_defaultNotificationLedOff))
+                + "";
+        mLedOffTime.setValue(ledOffTime);
+        Log.i(TAG, "led off time set to: " + ledOffTime);
+
+        mLedOnTime = (ListPreference) findPreference(PREF_LED_ON);
+        mLedOnTime.setOnPreferenceChangeListener(this);
+        String ledOnTime = Settings.System
+                .getInt(getActivity().getContentResolver(),
+                        Settings.System.NOTIFICATION_LIGHT_ON,
+                        getActivity().getResources().getInteger(
+                                com.android.internal.R.integer.config_defaultNotificationLedOn))
+                + "";
+        mLedOnTime.setValue(ledOnTime);
+        Log.i(TAG, "led on time set to: " + ledOnTime);
+
+        mColorPicker = (ColorPickerPreference) findPreference(PREF_COLOR_PICKER);
+        mColorPicker.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+        // if (preference == mColorPicker) {
+        //
+        // }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean result = false;
+
+        if (preference == mLedOffTime) {
+
+            int val = Integer.parseInt((String) newValue);
+            Log.i(TAG, "led off time new value: " + val);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_OFF, val);
+
+        } else if (preference == mLedOnTime) {
+
+            int val = Integer.parseInt((String) newValue);
+            Log.i(TAG, "led on time new value: " + val);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_ON, val);
+        } else if (preference == mColorPicker) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hex);
+
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_COLOR, intHex);
+            Log.e("ROMAN", intHex + "");
+        }
+
+        return result;
+    }
+
+}

Austrie/android_packages_apps_LiquidControl

Conflicts:
	AndroidManifest.xml
	res/xml/prefs_navbar.xml
	res/xml/prefs_statusbar_toggles.xml
	src/com/liquid/control/LiquidActivity.java
	src/com/liquid/control/activities/LED.java
	src/com/liquid/control/activities/Lockscreens.java
	src/com/liquid/control/activities/Navbar.java
	src/com/liquid/control/activities/NavbarLayout.java
	src/com/liquid/control/activities/PowerMenu.java
	src/com/liquid/control/activities/PowerSaver.java
	src/com/liquid/control/activities/StatusBarBattery.java
	src/com/liquid/control/activities/StatusBarClock.java
	src/com/liquid/control/activities/StatusBarGeneral.java
	src/com/liquid/control/activities/StatusBarToggles.java
	src/com/liquid/control/activities/TogglesLayout.java
	src/com/liquid/control/activities/UserInterface.java

src/com/liquid/control/ButtonBarHandler.java
@@ -1,0 +1,30 @@
+
+package com.liquid.control;
+
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.widget.Button;
+
+/**
+ * Interface letting {@link SettingsPreferenceFragment} access to bottom bar inside
+ * {@link android.preference.PreferenceActivity}.
+ */
+public interface ButtonBarHandler {
+    public boolean hasNextButton();
+
+    public Button getNextButton();
+}

src/com/liquid/control/DialogCreatable.java
@@ -1,0 +1,30 @@
+package com.liquid.control;
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+import android.app.Dialog;
+
+/**
+ * Letting the class, assumed to be Fragment, create a Dialog on it. Should be useful
+ * you want to utilize some capability in {@link SettingsPreferenceFragment} but don't want
+ * the class inherit the class itself (See {@link ProxySelector} for example).
+ */
+public interface DialogCreatable {
+
+    public Dialog onCreateDialog(int dialogId);
+}

src/com/liquid/control/fragments/Led.java
@@ -1,0 +1,100 @@
+
+package com.liquid.control.fragments;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.liquid.control.R;
+import com.liquid.control.SettingsPreferenceFragment;
+
+public class Led extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    public static final String TAG = "LEDPreferences";
+    private static final String PREF_LED_OFF = "led_off";
+    private static final String PREF_LED_ON = "led_on";
+    private static final String PREF_COLOR_PICKER = "led_color";
+
+    ListPreference mLedOffTime;
+    ListPreference mLedOnTime;
+    ColorPickerPreference mColorPicker;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.prefs_led);
+
+        mLedOffTime = (ListPreference) findPreference(PREF_LED_OFF);
+        mLedOffTime.setOnPreferenceChangeListener(this);
+        String ledOffTime = Settings.System
+                .getInt(getActivity().getContentResolver(),
+                        Settings.System.NOTIFICATION_LIGHT_OFF,
+                        getActivity()
+                                .getResources()
+                                .getInteger(
+                                        com.android.internal.R.integer.config_defaultNotificationLedOff))
+                + "";
+        mLedOffTime.setValue(ledOffTime);
+        Log.i(TAG, "led off time set to: " + ledOffTime);
+
+        mLedOnTime = (ListPreference) findPreference(PREF_LED_ON);
+        mLedOnTime.setOnPreferenceChangeListener(this);
+        String ledOnTime = Settings.System
+                .getInt(getActivity().getContentResolver(),
+                        Settings.System.NOTIFICATION_LIGHT_ON,
+                        getActivity().getResources().getInteger(
+                                com.android.internal.R.integer.config_defaultNotificationLedOn))
+                + "";
+        mLedOnTime.setValue(ledOnTime);
+        Log.i(TAG, "led on time set to: " + ledOnTime);
+
+        mColorPicker = (ColorPickerPreference) findPreference(PREF_COLOR_PICKER);
+        mColorPicker.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+            Preference preference) {
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean result = false;
+
+        if (preference == mLedOffTime) {
+
+            int val = Integer.parseInt((String) newValue);
+            Log.i(TAG, "led off time new value: " + val);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_OFF, val);
+
+        } else if (preference == mLedOnTime) {
+
+            int val = Integer.parseInt((String) newValue);
+            Log.i(TAG, "led on time new value: " + val);
+            result = Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_ON, val);
+        } else if (preference == mColorPicker) {
+            String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
+                    .valueOf(newValue)));
+            preference.setSummary(hex);
+
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NOTIFICATION_LIGHT_COLOR, intHex);
+            Log.e("LIQUID", intHex + "");
+        }
+
+        return result;
+    }
+}
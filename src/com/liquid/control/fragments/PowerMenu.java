152f23b9458a29ee78d92f6a58074ab12ec40e11
JBirdVegas <jbirdvegas@gmail.com>
2012-02-20 00:15:56 -0500

    PowerMenu: fix fullscreen mode not being detected properly and update toggles when appropriate

--- src/com/liquid/control/fragments/PowerMenu.java
+++ src/com/liquid/control/fragments/PowerMenu.java
@@ -13,13 +13,27 @@ import com.liquid.control.R;
 
 public class PowerMenu extends PreferenceFragment {
 
+    private static final String PREF_FULLSCREEN = "show_fullscreen";
     private static final String PREF_POWER_SAVER = "show_power_saver";
     private static final String PREF_SCREENSHOT = "show_screenshot";
     private static final String PREF_EASTER_EGG = "show_easter_egg";
 
+    CheckBoxPreference mShowFullscreen;
     CheckBoxPreference mShowPowerSaver;
     CheckBoxPreference mShowScreenShot;
     CheckBoxPreference mShowEasterEgg;
+
+    @Override
+    public void onPause() {
+        // no one cares let it go
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        updateToggles();
+        super.onResume();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,29 +63,71 @@ public class PowerMenu extends PreferenceFragment {
         mShowEasterEgg = (CheckBoxPreference) findPreference(PREF_EASTER_EGG);
         mShowEasterEgg.setChecked(Settings.System.getInt(getActivity()
                 .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_EASTER_EGG, 0) == 1);
+
+        // Check if we are in fullscreen mode if so we freeze the setEnabled state
+        // so user doesn't accidentally completely disable normal mode
+        boolean mInFullscreenMode = false;
+        mInFullscreenMode = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWER_DIALOG_FULLSCREEN, 0) == 1;
+
+        // now that we know what state we are in lets act accordingly
+        mShowFullscreen = (CheckBoxPreference) findPreference(PREF_FULLSCREEN);
+        mShowFullscreen.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_FULLSCREEN, 0) == 1);
+        mShowFullscreen.setEnabled(mInFullscreenMode == false);
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
+        boolean handled = false;
         if (preference == mShowPowerSaver) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_POWER_SAVER,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
+            handled = true;
         } else if (preference == mShowScreenShot) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_SCREENSHOT,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
+            handled = true;
         } else if (preference == mShowEasterEgg) {
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.POWER_DIALOG_SHOW_EASTER_EGG,
                     ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
-            return true;
+            handled = true;
+        } else if (preference == mShowFullscreen) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_DIALOG_SHOW_FULLSCREEN,
+                    ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+            handled = true;
         }
-
+        updateToggles();
+        if (handled) return true;
         return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public void updateToggles() {
+        // any time we do anything we call this
+        int powerSaverVal = 0;
+        mShowPowerSaver = (CheckBoxPreference) findPreference(PREF_POWER_SAVER);
+        try {
+            powerSaverVal = Settings.Secure.getInt(getActivity()
+                    .getContentResolver(), Settings.Secure.POWER_SAVER_MODE);
+        } catch (SettingNotFoundException e) {
+            mShowPowerSaver.setSummary("Enable power saver before you can see it in the menu.");
+        }
+        mShowPowerSaver.setChecked(powerSaverVal == 1);
+        mShowScreenShot.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_SCREENSHOT, 1) == 1);
+        mShowEasterEgg.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_EASTER_EGG, 0) == 1);
+        boolean mInFullscreenMode = false;
+        mInFullscreenMode = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.POWER_DIALOG_FULLSCREEN, 0) == 1;
+        mShowFullscreen.setChecked(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.POWER_DIALOG_SHOW_FULLSCREEN, 0) == 1);
+        mShowFullscreen.setEnabled(mInFullscreenMode == false);
     }
 }
 

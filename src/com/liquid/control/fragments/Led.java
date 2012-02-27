92bf6298fdf2ce00272d276031dbbf07e7fd4e29
Liquid Smooth <liquid0624@gmail.com>
2012-02-27 08:24:53 -0500

    Control Led Flash While Screen On (netboy3)

--- src/com/liquid/control/fragments/Led.java
+++ src/com/liquid/control/fragments/Led.java
@@ -5,6 +5,7 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
@@ -19,10 +20,12 @@ public class Led extends SettingsPreferenceFragment implements OnPreferenceChang
     private static final String PREF_LED_OFF = "led_off";
     private static final String PREF_LED_ON = "led_on";
     private static final String PREF_COLOR_PICKER = "led_color";
+    private static final String PREF_LED_SCREEN_ON = "led_screen_on";
 
     ListPreference mLedOffTime;
     ListPreference mLedOnTime;
     ColorPickerPreference mColorPicker;
+    CheckBoxPreference mLedScreenOn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,26 +61,32 @@ public class Led extends SettingsPreferenceFragment implements OnPreferenceChang
         mColorPicker = (ColorPickerPreference) findPreference(PREF_COLOR_PICKER);
         mColorPicker.setOnPreferenceChangeListener(this);
 
+        mLedScreenOn = (CheckBoxPreference) findPreference(PREF_LED_SCREEN_ON);
+        mLedScreenOn.setChecked(Settings.Secure.getInt(getActivity().getContentResolver(),
+            Settings.Secure.LED_SCREEN_ON, 0) == 1);
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
-
+        if (preference == mLedScreenOn) {
+            boolean checked = ((CheckBoxPreference) preference).isChecked();
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.Secure.LED_SCREEN_ON, checked ? 1 : 0);
+            Log.i(TAG, "LED flash when screen ON is set to: " + checked);
+            return true;
+        }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         boolean result = false;
-
         if (preference == mLedOffTime) {
-
             int val = Integer.parseInt((String) newValue);
             Log.i(TAG, "led off time new value: " + val);
             result = Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NOTIFICATION_LIGHT_OFF, val);
-
         } else if (preference == mLedOnTime) {
 
             int val = Integer.parseInt((String) newValue);
@@ -88,13 +97,11 @@ public class Led extends SettingsPreferenceFragment implements OnPreferenceChang
             String hex = ColorPickerPreference.convertToARGB(Integer.valueOf(String
                     .valueOf(newValue)));
             preference.setSummary(hex);
-
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NOTIFICATION_LIGHT_COLOR, intHex);
             Log.e("LIQUID", intHex + "");
         }
-
         return result;
     }
 }

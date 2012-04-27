6b3cbf2bae563b69c74a96ed2f61074a63cfc36e
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2012-04-27 21:04:40 +0200

    Added Sense4 Style task switcher 2/2 also removed redundant theme derp

--- src/com/liquid/control/fragments/UserInterface.java
+++ src/com/liquid/control/fragments/UserInterface.java
@@ -51,6 +51,8 @@ public class UserInterface extends SettingsPreferenceFragment implements
     private static final String PREF_ROTATION_ANIMATION = "rotation_animation_delay";
     private static final String PREF_180 = "rotate_180";
     private static final String PREF_DISABLE_SCREENSHOT_SOUND = "screenshot_sound";
+    private static final String PREF_RECENT_APP_SWITCHER = "recent_app_switcher";
+
 
     CheckBoxPreference mAllow180Rotation;
     ListPreference mAnimationRotationDelay;
@@ -59,11 +61,7 @@ public class UserInterface extends SettingsPreferenceFragment implements
     Preference mCustomLabel;
     CheckBoxPreference mDisableBootAnimation;
     CheckBoxPreference mDisableBugMailer;
-    CheckBoxPreference mHorizontalAppSwitcher;
-    CheckBoxPreference mShowImeSwitcher;
-    CheckBoxPreference mEnableVolumeOptions;
-    CheckBoxPreference mLongPressAppTasker;
-    CheckBoxPreference mDisableScreenshotSound;
+    ListPreference mRecentAppSwitcher;
     String mCustomLabelText = null;
 
     @Override
@@ -101,9 +99,11 @@ public class UserInterface extends SettingsPreferenceFragment implements
         mAllow180Rotation.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.ACCELEROMETER_ROTATION_ANGLES, (1 | 2 | 8)) == (1 | 2 | 4 | 8));
 
-        mHorizontalAppSwitcher = (CheckBoxPreference) findPreference("horizontal_recents_task_panel");
-        mHorizontalAppSwitcher.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.RECENT_APP_SWITCHER, 0) == 1);
+        mRecentAppSwitcher = (ListPreference) findPreference(PREF_RECENT_APP_SWITCHER);
+        mRecentAppSwitcher.setOnPreferenceChangeListener(this);
+        mRecentAppSwitcher.setValue(Integer.toString(Settings.System.getInt(getActivity()
+                .getContentResolver(), Settings.System.RECENT_APP_SWITCHER,
+                0)));
 
         mDisableBootAnimation = (CheckBoxPreference) findPreference("disable_bootanimation");
         mDisableBootAnimation.setChecked(!new File("/system/media/bootanimation.zip").exists());
@@ -214,13 +214,8 @@ public class UserInterface extends SettingsPreferenceFragment implements
                     Settings.System.ACCELEROMETER_ROTATION_ANGLES, checked ? (1 | 2 | 4 | 8)
                             : (1 | 2 | 8));
             return true;
-        } else if (preference == mHorizontalAppSwitcher) {
-            boolean checked = ((CheckBoxPreference) preference).isChecked();
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.RECENT_APP_SWITCHER, checked ? 1
-                            : 0);
-            Helpers.restartSystemUI();
-            return true;
+
+
         } else if (preference == mDisableBootAnimation) {
             boolean checked = ((CheckBoxPreference) preference).isChecked();
             if (checked) {
@@ -273,6 +268,13 @@ public class UserInterface extends SettingsPreferenceFragment implements
                     Settings.System.ACCELEROMETER_ROTATION_SETTLE_TIME,
                     Integer.parseInt((String) newValue));
             handled = true;
+            
+        } else if (preference == mRecentAppSwitcher) {
+            int val = Integer.parseInt((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.RECENT_APP_SWITCHER, val);
+            Helpers.restartSystemUI();
+            return true;
         }
 
         //update our dynamic values and return if we handled
